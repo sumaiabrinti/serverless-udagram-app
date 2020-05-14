a8a60b7fc552b429dc134b58e7ae9f0b2ab5936f
sumaia.jahan <sumaia.jahan@grameenphone.com>
2020-05-14 10:26:03 +0600

    config.ts modified

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,7 +3,6 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   domain: 'dev-7bg5wtuf.auth0.com',            // Auth0 domain
- // clientId: '0IY1eBpGfRS2vT9Tr3Bk0dsn2S2RDS5K',
   clientId: 'kvXmSS9S21Rfhmg3iFdWgZ6kGECpG2mZ',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
