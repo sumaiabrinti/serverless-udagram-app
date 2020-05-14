9ce0d81f76642e825cbdd9fb0d440b20d632c8df
sumaia.jahan <sumaia.jahan@grameenphone.com>
2020-05-14 10:21:16 +0600

    serverless app

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+const apiId = 'gqynkuqll4'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-7bg5wtuf.auth0.com',            // Auth0 domain
+ // clientId: '0IY1eBpGfRS2vT9Tr3Bk0dsn2S2RDS5K',
+  clientId: 'kvXmSS9S21Rfhmg3iFdWgZ6kGECpG2mZ',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
